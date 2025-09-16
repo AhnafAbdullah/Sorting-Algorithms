927dc2045fee8776c42eb6b34242e701e9b55cf4
AhnafAbdullah <ahnaf4909@gmail.com>
2025-09-16 19:47:30 +0500

    Added chrono in merge sort for time

--- Merge_Sort.cpp
+++ Merge_Sort.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <chrono>   
 using namespace std;
 
 void merge(vector<int>& array, int low, int mid, int high) {
     vector<int> arr1(array.begin() + low, array.begin() + mid + 1);
-    vector<int> arr2(array.begin() + mid + 1, array.begin() + high + 1); 
+    vector<int> arr2(array.begin() + mid + 1, array.begin() + high + 1);
 
     int i = 0, j = 0, k = low;
     while (i < (int)arr1.size() && j < (int)arr2.size()) {
-        if (arr1[i] <= arr2[j]) {        
+        if (arr1[i] <= arr2[j]) {
             array[k++] = arr1[i++];
         }
         else {
@@ -30,9 +31,21 @@ void mergeSort(vector<int>& array, int low, int high) {
 
 int main() {
     vector<int> arr = { 5, 3, 8, 3, 9, 1, 5, 2 };
+
+    auto start = chrono::high_resolution_clock::now();
+
     mergeSort(arr, 0, (int)arr.size() - 1);
 
+    auto stop = chrono::high_resolution_clock::now();
+
+    // calculate duration in microseconds
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+	
+    cout << "Sorted array: ";
     for (int num : arr) cout << num << " ";
-    cout << '\n';
+    cout << "\n";
+
+    cout << "Time taken by mergeSort: " << duration.count() << " microseconds\n";
+
     return 0;
 }
